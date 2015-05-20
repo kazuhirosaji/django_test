f112672cb98caffd9987c200388662ce2746b148
kazuhirosaji <kazuhirosaji@gmail.com>
2015-05-20 22:50:26 +0900

    add template page

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,12 @@
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 
 def helloworld(request):
     return HttpResponse('Hello world!')
+
+def mytemplate(request):
+	t = get_template('template1.html')
+	html = t.render(Context({'myname': 'Saji'}))
+	return HttpResponse(html)
+
