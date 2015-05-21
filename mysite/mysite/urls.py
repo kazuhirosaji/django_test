e2373af248f686348d1f2c7877dbdbb60f5d252e
kazuhirosaji <kazuhirosaji@gmail.com>
2015-05-22 00:15:02 +0900

    template inheritance

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, include, url
 from mysite.views import helloworld
 from mysite.views import mytemplate
 
-urlpatterns = patterns('',
-    url(r'^helloworld/', helloworld),
-)
+# urlpatterns = patterns('',
+#     url(r'^helloworld/', helloworld),
+# )
 
 urlpatterns = patterns('',
 	('^templatetest1/$', mytemplate)
